76bf2f55848af5d2bd1086addfe394c9b50bd812
Park-Ping <support@parkping.co.in>
2026-02-05 23:35:56 +0530

    Update BrandHeader.jsx

--- src/pages/authentication-gateway/components/BrandHeader.jsx
+++ src/pages/authentication-gateway/components/BrandHeader.jsx
@@ -5,7 +5,7 @@ const BrandHeader = () => {
       <div className="flex justify-center mb-4 md:mb-6">
         <div className="inline-flex items-center justify-center w-16 h-16 md:w-20 md:h-20 rounded-2xl bg-primary/10">
           <img
-            src="/logo.png"
+            src="/ParkPing Logo.png"
             alt="ParkPing Logo"
             className="w-10 h-10 md:w-12 md:h-12 object-contain"
           />
@@ -26,3 +26,4 @@ const BrandHeader = () => {
 };
 
 export default BrandHeader;
+
